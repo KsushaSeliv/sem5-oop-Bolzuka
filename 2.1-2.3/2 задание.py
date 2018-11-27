12d04100680f1ac235cea5ef4883d4f207081651
Shibaeva Maria <Bolzuka@gmail.com>
2018-11-27 20:53:34 +0300

    Update 2 задание.py

--- 2.1-2.3/2 задание.py
+++ 2.1-2.3/2 задание.py
@@ -4,3 +4,50 @@
 # 2.3. Формирование отчета по практическому заданию и публикация его в портфолио.
 # Отчет сформирован в портфолио по данным заданиям.
 
+class Post:
+    def __init__(self, author, content):
+        self.author = author
+        self._content = content
+        self.comments = list()
+
+    def show(self):
+        return ("Автор: " + str(self.author) + "\n" + "Содержание: " + str(self._content) + "\n")
+
+    def show_comments(self):
+        comments_string = ""
+        for comment in self.comments:
+            comments_string += comment.show()
+        return comments_string
+
+    @property
+    def content(self):
+        return self._content
+
+    def add_comment(self, comment):
+        self.comments.append(comment)
+
+    @content.setter
+    def content(self, content):
+        self._content = content
+
+    @content.getter
+    def content(self):
+        return self._content
+
+      
+class Comments(Post):
+    def __init__(self, author, content):
+        super(Comments, self).__init__(author=author, content=content)
+
+
+if __name__ == "__main__":
+    post = Post("Эрмитаж", "Мы вынуждены вас расстроить, наших любимых гостей. Сегодня в Эрмитаже санитарный день, поэтому мы закрыты, но уже завтра мы будем работать по расписанию")
+
+    post.add_comment(Comments("Кирилл", "Я так хотел посетить сегодня эрмитаж"))
+    post.add_comment(Comments("Ксюша", "Эх, придется идти завтра, хочу к импрессионистам на выставку :)"))
+
+    with open('Record.txt', 'a') as f:
+        f.write("\nПост\n")
+        f.write(str(post.show()))
+        f.write("\nКомментарии\n")
+        f.write(str(post.show_comments()))
